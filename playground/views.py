e77523cec55ab196916653ea4ebbf5e3da6a0ea4
Moosa-Origano <musa9islam@gmail.com>
2024-11-09 17:44:38 +0000

    Initial commit

---
+++ playground/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# request --> response
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
+def say_hello(request):
+    # Do pretty much anything
+    x = calculate()
+    return render(request, "hello.html", {'name': 'Marsh'})
